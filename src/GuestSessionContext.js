7be5af23f2c36f338fdcaadcccbe34361d130139
Anton Chernetsov <anton.chernetsov@gmail.com>
2023-04-18 03:38:20 +0300

    fixed

--- src/GuestSessionContext.js
+++ src/GuestSessionContext.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ErrorIndicator from './components/error-indicator'
+import { Spin } from 'antd'
 
 export const GuestSessionContext = React.createContext()
 
@@ -27,30 +29,23 @@ export class GuestSessionProvider extends React.Component {
 
   async getSession() {
     const session = await this.getResource(
-      `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=a0ebd979d0247d439d1914491e74f506`
-    ).catch((err) => {
-      console.error('Status:', err.status, 'Error:', err)
-      this.setState({ error: true })
-    })
-
-    if (session.success) {
-      // Get the genres if they haven't been cached yet
-      if (this.state.genres === null) {
-        const genres = await this.getResource(
-          'https://api.themoviedb.org/3/genre/movie/list?api_key=a0ebd979d0247d439d1914491e74f506&language=en-US'
-        ).catch((err) => {
-          console.error('Status:', err.status, 'Error:', err)
+      `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=aa0ebd979d0247d439d1914491e74f506`
+    ).catch(this.onError)
+    if (session) {
+      if (session.success) {
+        if (this.state.genres === null) {
+          const genres = await this.getResource(
+            'https://api.themoviedb.org/3/genre/movie/list?api_key=a0ebd979d0247d439d1914491e74f506&language=en-US'
+          ).catch(this.onError)
+          if (genres) this.setState({ genres: genres.genres })
+          else this.onError()
+        }
+        this.setState({
+          guestSessionId: session.guest_session_id,
+          loading: false,
         })
-        this.setState({ genres: genres.genres })
-      }
-
-      this.setState({
-        guestSessionId: session.guest_session_id,
-        loading: false,
-      })
-    } else {
-      this.setState({ error: true })
-    }
+      } else this.onError()
+    } else this.onError()
   }
 
   componentDidMount() {
@@ -58,6 +53,19 @@ export class GuestSessionProvider extends React.Component {
   }
 
   render() {
-    return <GuestSessionContext.Provider value={this.state}>{this.props.children}</GuestSessionContext.Provider>
+    const { loading, error } = this.state
+    const hasData = !(loading || error)
+    const errorMessage = error ? <ErrorIndicator error={error} /> : null
+    const spinner = loading ? <Spin className="spinner" size="large" /> : null
+
+    return (
+      <>
+        {errorMessage}
+        {spinner}
+        {hasData ? (
+          <GuestSessionContext.Provider value={this.state}>{this.props.children}</GuestSessionContext.Provider>
+        ) : null}
+      </>
+    )
   }
 }
--- src/components/error-indicator/ErrorIndicator.js
+++ src/components/error-indicator/ErrorIndicator.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import './ErrorIndicator.css'
 import { Alert } from 'antd'
+import './crash_img.jpg'
 
 const ErrorIndicator = (error) => {
   console.log(error)
   return (
-    <Alert
-      className="error-indicator"
-      type="error"
-      message="Error"
-      description="Something went wrong! Despite everything Tom Cruise and Chip & Dale are already rushing to fix it!"
-      banner
-    />
+    <>
+      <Alert
+        className="error-indicator"
+        type="error"
+        message="Error"
+        description="Something went wrong! Despite everything Tom Cruise and Chip & Dale are already rushing to fix it!"
+        banner
+      />
+      <img className="error-poster" src="../error-indicator/crash_img.jpg" alt="funny poster"></img>
+    </>
   )
 }
 
